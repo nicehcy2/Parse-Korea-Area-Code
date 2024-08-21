a79c5fcef634e3371588dce918e88172160079df
nicehcy <nicehcy2@naver.com>
2024-08-21 16:39:53 +0900

    feat: 법정동코드 폐지 데이터 필터

---
+++ filter_nonexistent_lines.py
@@ -0,0 +1,11 @@
+# 원본 파일에서 각 줄의 뒤에서 3칸의 문자열이 "미존재"가 아닌 줄만 새로운 파일에 저장
+input_filename = "법정동코드 전체자료.txt"
+output_filename = "법정동코드 전체자료_미존재 제거 버전.txt"
+
+# 파일 입출력
+with open(input_filename, "r") as infile, open(output_filename, "w") as outfile:
+    for line in infile:
+        # 각 줄의 공백을 제거하고 마지막 3글자 확인
+        if not line.strip()[-2:] == "폐지":
+            # 조건에 맞는 줄을 새로운 파일에 작성
+            outfile.write(line)
